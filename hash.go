26343b13b4d514258f0e3baff06108b9ea6189c6
Kaveh Goodarzi <kaveh4.voll@gmail.com>
2026-01-08 17:43:10 +0330

    Add hash file

---
+++ hash.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"os"
+)
+
+func HashFile(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hasher := sha256.New()
+
+	if _, err := io.Copy(hasher, file); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
