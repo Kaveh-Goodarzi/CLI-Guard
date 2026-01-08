46389dee6afe859fbef2f31c55b9edfd1dc1582a
Kaveh Goodarzi <kaveh4.voll@gmail.com>
2026-01-08 17:53:37 +0330

    finish base of the CLI-Guard

--- verify.go
+++ verify.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+func Init(root string) error {
+	files, err := ScanFiles(root)
+	if err != nil {
+		return err
+	}
+
+	manifest := make(Manifest)
+
+	for _, file := range files {
+		hash, err := HashFile(file)
+		if err != nil {
+			return err
+		}
+
+		rel, _ := filepath.Rel(root, file)
+		manifest[rel] = hash
+	}
+
+	return WriteManifest(root, manifest)
+}
+
+func Verify(root string) error {
+	stored, err := ReadManifest(root)
+	if err != nil {
+		return err
+	}
+
+	currentFiles, err := ScanFiles(root)
+	if err != nil {
+		return err
+	}
+
+	current := make(map[string]string)
+
+	for _, file := range currentFiles {
+		hash, err := HashFile(file)
+		if err != nil {
+			return err
+		}
+
+		rel, _ := filepath.Rel(root, file)
+		current[rel] = hash
+	}
+
+	for path, oldHash := range stored {
+		newHash, ok := current[path]
+		if !ok {
+			fmt.Println(path, "REMOVED")
+			continue
+		}
+
+		if oldHash == newHash {
+			fmt.Println(path, "OK")
+		} else {
+			fmt.Println(path, "FAIL")
+		}
+	}
+
+	for path := range current {
+		if _, ok := stored[path]; !ok {
+			fmt.Println(path, "ADDED")
+		}
+	}
+
+	return nil
+}
